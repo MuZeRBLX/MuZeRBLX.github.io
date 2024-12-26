bcaf9b87b34bbdfe80a18181d4515178c1f80cde
MuZeRBLX <112740839+MuZeRBLX@users.noreply.github.com>
2024-12-25 21:17:24 -0600

    Create Learning.js

---
+++ Learning.js
@@ -0,0 +1,46 @@
+let list = [{Username:"Yessir",Password:"Yessir"},{Username:"MuZe",Password:"password123"}]
+
+function DoLogin() {
+
+    const UInput = document.getElementById("InputUsername")
+    const PInput = document.getElementById("InputPassword")
+
+    const Username = UInput.value
+    const Password = PInput.value
+
+    var CurrentState = 0
+
+    console.log(Username)
+    console.log(Password)
+    
+    for (let v of list) {
+
+    if (Username === v.Username && Password === v.Password) {
+
+        console.log("Loggin in")
+        CurrentState = 1
+    }else
+    {console.log("invalid Credentials")
+}
+
+    if (CurrentState == 0) {
+
+        document.getElementById("CreateButton").innerText = "Invalid Credentials"
+    setTimeout(() => {document.getElementById("CreateButton").innerText = "Login";},1500)
+
+    }else if (CurrentState == 1) {
+
+        document.getElementById("CreateButton").innerText = "You Logged In!"
+        setTimeout(() => {document.getElementById("CreateButton").innerText = "Login";},1500)
+
+    }
+
+}
+
+};
+
+document.getElementById("CreateButton").onclick = ()=>{
+
+    DoLogin()
+
+}
